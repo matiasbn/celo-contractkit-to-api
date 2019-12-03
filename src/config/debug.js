fcb2bbe3c9814b66cc641bda24bf1af6f82c0940
Matias Barrios <matias.barriosn@gmail.com>
2019-12-03 15:29:52 -0300

    feat: debug added

---
+++ src/config/debug.js
@@ -0,0 +1,13 @@
+import debug from 'debug'
+
+// Set debug namespace for our app
+// https://github.com/visionmedia/debug/issues/117
+const debugMongoose = debug('mongoose')
+const debugWallet = debug('wallet')
+const debugTransacion = debug('transaction')
+
+export {
+  debugMongoose,
+  debugWallet,
+  debugTransacion,
+}
