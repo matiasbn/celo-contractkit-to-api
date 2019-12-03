83c8fa0c186a1dbadd90f08db7b563eeeb10b359
Matias Barrios <matias.barriosn@gmail.com>
2019-12-03 17:21:47 -0300

    chore: debugMongoose to debugApp

--- src/config/debug.js
+++ src/config/debug.js
@@ -2,13 +2,13 @@ import debug from 'debug'
 
 // Set debug namespace for our app
 // https://github.com/visionmedia/debug/issues/117
-const debugMongoose = debug('mongoose')
+const debugApp = debug('mongoose')
 const debugWallet = debug('wallet')
 const debugTransacion = debug('transaction')
 const debugStorage = debug('storage')
 
 export {
-  debugMongoose,
+  debugApp,
   debugWallet,
   debugTransacion,
   debugStorage,
