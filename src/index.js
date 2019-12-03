675c2acdecebc8f0d702b399878d07378820bf4d
Matias Barrios <matias.barriosn@gmail.com>
2019-12-03 14:17:54 -0300

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { newKit } from '@celo/contractkit'
+
+const keySize = 32
+
+// Create wallet
+const kit = newKit('https://alfajores-forno.celo-testnet.org/')
+const { web3 } = kit
+
+// Create crypto key for AES
+const crypto = (web3.utils.randomHex(keySize)).replace('0x', '')
+
+// Create random seed for wallet
+const randomSeed = (web3.utils.randomHex(keySize)).replace('0x', '')
+
+// Create wallet
+const wallet = web3.eth.accounts.wallet.create(1, randomSeed)
+
+//
+console.log(wallet)
