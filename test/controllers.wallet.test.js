b0dbad462c7d71f843d438c839ba00114e1a1b15
Matias Barrios <matias.barriosn@gmail.com>
2019-12-03 22:23:51 -0300

    chore: test file corrected

--- test/controllers.wallet.test.js
+++ test/controllers.wallet.test.js
@@ -1,6 +1,4 @@
 import request from 'supertest'
-import appRootPath from 'app-root-path'
-import dotenv from 'dotenv'
 import Controller from '../src/controllers/wallet'
 import app from '../src/config/express'
 import initDB from '../src/config/db'
@@ -9,8 +7,14 @@ beforeAll(() => {
   initDB(app)
 })
 
-describe('Testing wallet controller', () => {
-  dotenv.config({ path: `${appRootPath.path}/test/.env` })
+describe('Wallet controller unit tests', () => {
+  it('should return a 200', async () => {
+    const response = await request(app).post('/wallet/create')
+    expect(response.status).toBeDefined()
+  })
+})
+
+describe('Wallet controller integration tests', () => {
   it('should return a 200', async () => {
     const response = await request(app).post('/wallet/create')
     expect(response.status).toBeDefined()
