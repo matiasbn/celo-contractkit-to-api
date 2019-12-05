6e887df64c814a156495f48d40e1effa589dd42f
Matias Barrios <matias.barriosn@gmail.com>
2019-12-05 12:26:51 -0300

    feat: fetch route added

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -4,5 +4,6 @@ import Controller from '../controllers/wallet'
 const router = new Router()
 
 router.post('/create', Controller.createWallet)
+router.post('/fetch', Controller.fetchWallet)
 
 export default router
