9fef28a5893ca285f1581fe1f9c1e9999ecf2435
Matias Barrios <matias.barriosn@gmail.com>
2019-12-09 16:40:02 -0300

    chore: messages updated

--- src/common/error-messages.js
+++ src/common/error-messages.js
@@ -1,8 +1,10 @@
 const ERROR_MESSAGES = {
   EMAIL_IS_EMPTY: 'email cannot be empty',
   EMAIL_OR_PHONE_ALREADY_REGISTERED: 'email or phone already registered with a private key',
+  IS_NOT_EMAIL: 'email with bad format',
+  IS_NOT_PHONE: 'phone with bad format',
   PHONE_IS_EMPTY: 'phone cannot be empty',
-  PRIVATE_KEY_NOT_FOUND: 'private key not found for given pair emil-phone',
+  PRIVATE_KEY_NOT_FOUND: 'private key not found for given pair email-phone',
 }
 
 export default ERROR_MESSAGES
