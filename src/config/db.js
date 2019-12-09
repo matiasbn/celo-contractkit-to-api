5ae6b88e8ed2e1d71a0ed16f6108f258c8479359
Matias Barrios <matias.barriosn@gmail.com>
2019-12-09 12:54:44 -0300

    feat(mongoose): recognize if  express is necesasary

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ import bluebird from 'bluebird'
 import ms from 'ms'
 import http from 'http'
 import { debugMongo, debugExpress, debugTest } from './debug'
+import app from './express'
 
 const options = {
   promiseLibrary: bluebird,
@@ -20,6 +21,7 @@ class MongoClient {
     const { databaseName, appPort } = _options
     this.instance = null
     this.expressPort = appPort || process.env.APP_PORT || 3000
+    this.app = appPort && true
     this.database = databaseName || process.env.DATABASE_NAME || 'celopipol'
     this.mongoDatabaseUri = `${process.env.MONGO_URI}${this.database}` || 'mongodb://localhost/test'
     debugMongo('mongo-uri', this.mongoDatabaseUri)
@@ -28,34 +30,33 @@ class MongoClient {
     debugTest('api-port', this.expressPort)
   }
 
-  createInstance(app) {
-    mongoose
-      .connect(this.mongoDatabaseUri, options)
-      .then(() => debugMongo('Mongodb connected'))
-      .catch((error) => debugMongo(`Failed to connect to mongodb ${error.toString()}`))
-
-    mongoose
-      .connection
-      .once('open', () => {
-        if (app) {
-          const server = http.createServer(app)
-          debugMongo('Starting Express Server...')
-          debugTest('Starting Express Server...')
-          server.listen(this.expressPort, () => {
-            debugExpress(`Server running on port: ${this.expressPort}`)
-            debugTest(`Server running on port: ${this.expressPort}`)
-          })
-        }
-      })
-    this.instance = mongoose
-    return this.instance
+  async createInstance() {
+    try {
+      await mongoose.connect(this.mongoDatabaseUri, options)
+      debugMongo('Mongodb connected')
+      debugTest('Mongodb connected')
+      if (this.app) {
+        const server = http.createServer(app)
+        debugMongo('Starting Express Server...')
+        debugTest('Starting Express Server...')
+        server.listen(this.expressPort, () => {
+          debugExpress(`Server running on port: ${this.expressPort}`)
+          debugTest(`Server running on port: ${this.expressPort}`)
+        })
+      }
+      this.instance = mongoose
+      return this.instance
+    } catch (error) {
+      debugMongo(`Failed to connect to mongodb ${error.toString()}`)
+      debugTest(`Failed to connect to mongodb ${error.toString()}`)
+      return null
+    }
   }
 
-  getInstance(app) {
+  getInstance() {
     if (!this.instance) {
-      return this.createInstance(app)
+      return this.createInstance()
     }
-
     return this.instance
   }
 }
