e903efbf8e3e05c0505797bbf71c0f75edab85a7
Matias Barrios <matias.barriosn@gmail.com>
2020-01-02 10:40:10 -0300

    chore: backup

--- src/workers/transfer.js
+++ src/workers/transfer.js
@@ -6,7 +6,6 @@ import Transaction from '~/src/models/transactions'
 const cUSD = (privateKey, address, toAddress, amount) => new Promise(async (resolve, reject) => {
   const kit = newKit(process.env.CELO_URL)
   try {
-    // @todo watch concurrence by account to be 1
     const stableToken = await kit.contracts.getStableToken()
     const ether = kit.web3.utils.toWei(amount, 'ether')
     kit.addAccount(privateKey)
@@ -14,15 +13,10 @@ const cUSD = (privateKey, address, toAddress, amount) => new Promise(async (reso
       address, toAddress, amount, privateKey,
     }
     debugWorkers('transfer cUSD worker parameters: \n', params)
-    const tx = await stableToken.transfer(toAddress, ether).send({
-      from: address,
-    })
-    const txHash = await tx.getHash()
-    // @todo check if nonce is present on this receipt
-    await tx.waitReceipt()
-    const transaction = await kit.web3.eth.getTransaction(txHash)
+    const receipt = await stableToken.transfer(toAddress, ether).sendAndWaitForReceipt({ from: address })
+    const transaction = await kit.web3.eth.getTransaction(receipt.transactionHash)
     const { nonce } = transaction
-    const storedTransaction = await Transaction.create({ address, nonce, txHash })
+    const storedTransaction = await Transaction.create({ address, nonce, txHash: receipt.transactionHash })
     debugWorkers('stored transaction:\n ', storedTransaction)
     resolve()
   } catch (error) {
@@ -37,7 +31,6 @@ const cUSD = (privateKey, address, toAddress, amount) => new Promise(async (reso
 const cGLD = (privateKey, address, toAddress, amount) => new Promise(async (resolve, reject) => {
   const kit = newKit(process.env.CELO_URL)
   try {
-    // @todo watch concurrence by account to be 1
     const goldToken = await kit.contracts.getGoldToken()
     const ether = kit.web3.utils.toWei(amount, 'ether')
     kit.addAccount(privateKey)
@@ -45,15 +38,10 @@ const cGLD = (privateKey, address, toAddress, amount) => new Promise(async (reso
       address, toAddress, amount, privateKey,
     }
     debugWorkers('transfer cGLD worker parameters: \n', params)
-    const tx = await goldToken.transfer(toAddress, ether).send({
-      from: address,
-    })
-    const txHash = await tx.getHash()
-    // @todo check if nonce is present on this receipt
-    await tx.waitReceipt()
-    const transaction = await kit.web3.eth.getTransaction(txHash)
+    const receipt = await goldToken.transfer(toAddress, ether).sendAndWaitForReceipt({ from: address })
+    const transaction = await kit.web3.eth.getTransaction(receipt.transactionHash)
     const { nonce } = transaction
-    const storedTransaction = await Transaction.create({ address, nonce, txHash })
+    const storedTransaction = await Transaction.create({ address, nonce, txHash: receipt.transactionHash })
     debugWorkers('stored transaction:\n ', storedTransaction)
     resolve()
   } catch (error) {
