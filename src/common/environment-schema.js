f74d3902a721b417f164aa033b69ec598f54105d
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-01-03 13:38:54 -0300

    fix: app port required

--- src/common/environment-schema.js
+++ src/common/environment-schema.js
@@ -10,11 +10,13 @@ import Joi from '@hapi/joi'
 
 const environmentSchema = (environment) => {
   const joiString = Joi.string()
+  const joiNumber = Joi.number()
   return {
     NODE_ENV: joiString.required().valid('development', 'production', 'test').default('development'),
     MONGO_URI: joiString.required(),
     KEY_SIZE: joiString.required(),
     CELO_URL: joiString.required(),
+    APP_PORT: joiNumber.required(),
     ...environment === 'main' && { DATABASE_NAME: joiString.required() },
   }
 }
